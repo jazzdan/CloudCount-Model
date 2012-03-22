bdf9c71c3e6770f5d21011c9c4f98040875dcd62
Joey Carmello <Joeycarmello@gmail.com>
2012-03-21 23:23:09 -0400

    notes implementation

--- src/cloudcount/models/Note.java
+++ src/cloudcount/models/Note.java
@@ -9,7 +9,53 @@ import org.workplicity.entry.Entry;
  */
 public class Note extends Entry {
     
+    
+    // description text for the note
+    private String label;
+    
+    // Contents of the note
+    private String note;
+    
+    // 
+    private int created_by;
+    
+    // 
+    private Budget budget;
+    
     public Note() {
         
     }
+
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Budget budget) {
+        this.budget = budget;
+    }
+
+    public int getCreated_by() {
+        return created_by;
+    }
+
+    public void setCreated_by(int created_by) {
+        this.created_by = created_by;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
 }
